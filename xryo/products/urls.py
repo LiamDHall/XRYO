78c3d3f229a4556f893a21c67f5045f7e35456b4
LiamDHall <hall.d.liam@gmail.com>
2021-03-22 17:40:12 +0000

    Add and style product detail and variant detail pages
    Pages are copies of each other with slightly different templating logic

--- xryo/products/urls.py
+++ xryo/products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<product_id>/<variant_id>', views.product_variant, name='product_variant'),
 ]
--- xryo/products/views.py
+++ xryo/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Variant, ImageAlbum, Image
 
 # Create your views here.
@@ -22,3 +22,36 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to details of a single product
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    print(f'PRODUCT === {product}')
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
+
+def product_variant(request, product_id, variant_id):
+    """ A view to details of a single variant
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+    variant = get_object_or_404(Variant, pk=variant_id)
+
+    print(f'VARIANT === {variant}')
+    print(f'PRODUCT === {product}')
+
+    context = {
+        'variant': variant,
+        'product': product,
+    }
+
+    return render(request, 'products/product_variant.html', context)
