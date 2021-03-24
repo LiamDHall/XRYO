064d9c671645680e939ed6fd2be33aabc3f818c2
LiamDHall <hall.d.liam@gmail.com>
2021-03-24 20:43:51 +0000

    Add function to fix filter bar to site header after scrolling past it

--- products/static/products/js/products.js
+++ products/static/products/js/products.js
@@ -2,3 +2,20 @@
 $('.img-thumbnail').on('click', function() {
     $('.display-image').prop('src', this.src);
 });
+
+// Fix Filter to Site Header if Scrolled Pasted
+var filterBottom = $('.filters').offset().top
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    var siteHeaderHeight = $('.site-header').height() 
+    var filterTopPos = filterBottom - siteHeaderHeight
+    if(scroll > filterTopPos){
+        $('.filters').addClass('attach-to-header')
+        $('.filters').css('top', `${siteHeaderHeight}px`)
+    }
+    else {
+        $('.filters').removeClass('attach-to-header')
+        $('.filters').removeAttr("style");
+    }
+});
