3f3ee9a031e8fd804391c6e3d33d392bbe4ade7b
LiamDHall <hall.d.liam@gmail.com>
2021-04-06 12:53:36 +0000

    Add test to test response and template of home view

---
+++ home/test_view.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+class TestDjango(TestCase):
+
+    def test_homepage_get(self):
+        """ Tests response and if template is correct
+        """
+
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
