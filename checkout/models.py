6508114cadf4dba0ae92aef04c198afe8da261d0
LiamDHall <hall.d.liam@gmail.com>
2021-04-11 20:38:43 +0000

    Add further save functionality to checkout models and make migrations

--- checkout/models.py
+++ checkout/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
-
 from products.models import Product, Variant
 
 
@@ -19,15 +18,57 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    delivery_charge = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _create_order_number(self):
+        """ Create a random unique order number
+        """
+
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """ Update grand total when an item is added taking into
+        account delivery charge.
+        """
+
+        self.order_total = self.items.aggregate(Sum('item_total'))['item_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_charge = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_charge = 0
+        self.grand_total = self.order_total + self.delivery_charge
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """Replace the original save method to add addition function.
+        Sets the order number if not already set.
+        """
+
+        if not self.order_number:
+            self.order_number = self._create_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
+
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     variant = models.ForeignKey(Variant, null=False, blank=False, on_delete=models.CASCADE)
     product_size = models.CharField(max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """ Replace the original save method to add addition function.
+        Sets the item total and update the order total.
+        """
+
+        self.item_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
