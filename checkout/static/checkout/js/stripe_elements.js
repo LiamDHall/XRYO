d15338bb177c82b26c56c44787f8972a1566f90b
LiamDHall <hall.d.liam@gmail.com>
2021-04-13 15:26:33 +0000

    Add stripe payment basic functionality to checkout form

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,12 +1,11 @@
 /*
     Stripe Element 
-    Soruce: https://stripe.com/docs/payments/accept-a-payment
+    Soruce: https://stripe.com/docs/payments/accept-a-payment-synchronously
     Apdated style to match the rest of the form it will fit
     into.
 */
-
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -26,3 +25,59 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Validation Error Handler when the Card Element Input changes
+card.on('change', function (event) {
+    var errorContainer = document.getElementById('card-errors');
+    if (event.error) {
+        var errorMessage = `
+            <span class="ml-2" role="alert"><i class="fas fa-times"></i></span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorContainer).html(errorMessage);
+        $('#card-element').removeClass('mb-5');
+    }
+    
+    else {
+        errorContainer.textContent = '';
+        $('#card-element').addClass('mb-5');
+    }
+});
+
+/* 
+    Form Submit Handler
+    Source: https://stripe.com/docs/payments/accept-a-payment-synchronously
+    Adpated for my needs.
+*/
+var form = document.getElementById('checkout-form');
+
+form.addEventListener('submit', function(ev) {
+
+    // Stops the form submitting
+    ev.preventDefault();
+
+    // Prevent multiple submissions
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true);
+
+
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorContainer = document.getElementById('card-errors');
+            var errorMessaage = `
+                <span role="alert"><i class="fas fa-times"></i></span>
+                <span>${result.error.message}</span>`;
+            $(errorContainer).html(errorMessaage);
+            card.update({'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,9 @@ from django.conf import settings
 
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
@@ -20,11 +23,25 @@ def checkout(request):
         messages.error(request, "Can't Checkout: Your Bag is Empty")
         return redirect(reverse('products'))
 
+    current_bag = bag_contents(request)
+    bag_total = current_bag['bag_grand_total']
+
+    # Round total as Stripe charges in pennies
+    # ie. £23.51 is equal to 2351p
+    stripe_total = round(bag_total * 100)
+    stripe.api_key = stripe_secret_key
+
+    # Create Payment Intent
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'stripe_client_secret': 'test key test',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
