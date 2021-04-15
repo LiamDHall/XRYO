10f412dc17f5cab80f52075c155041d6f6f7a14f
LiamDHall <hall.d.liam@gmail.com>
2021-04-15 19:07:12 +0000

    Update country input to be a dropdown selector to match stripe format
    This commit will also reformat lines that were too long and the
    order of model/form fields.

--- checkout/forms.py
+++ checkout/forms.py
@@ -12,8 +12,8 @@ class OrderForm(forms.ModelForm):
             'street_address1',
             'street_address2',
             'town_or_city',
-            'postcode',
             'county',
+            'postcode',
             'country',
         )
 
@@ -30,17 +30,17 @@ class OrderForm(forms.ModelForm):
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'town_or_city': 'Town or City',
-            'postcode': 'Postal Code',
             'county': 'County',
-            'country': 'Country',
+            'postcode': 'Postal Code',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-customise'
             self.fields[field].label = False
