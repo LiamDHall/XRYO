459cf22015d85c3b65ed79ad8146662914994783
LiamDHall <hall.d.liam@gmail.com>
2021-04-16 13:26:02 +0000

    Add profile delivery form to template and update view to handle it

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import Profile
+from .forms import ProfileForm
 
 
 def profile(request):
     """ View to display a users profile.
     """
 
-    context = {
+    # Get user profile
+    profile = get_object_or_404(Profile, user=request.user)
 
+    # POST / Save Form
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile details saved')
+
+    # Get saved user detailed
+    form = ProfileForm(instance=profile)
+
+    # Get all users orders
+    orders = profile.orders.all()
+
+    context = {
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, 'profiles/profiles.html', context)
