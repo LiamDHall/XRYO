45e5f9c84733eee677bde8e1282b33e0e4ccae2d
LiamDHall <hall.d.liam@gmail.com>
2021-04-26 15:28:58 +0000

    Add script to show image names that will be uploaded on product form

--- products/static/products/js/pm_variant_inputs.js
+++ products/static/products/js/pm_variant_inputs.js
@@ -8,11 +8,16 @@ $('#add-variant').on('click', function(){
         <fieldset id="variant-${variantAmount + 1}" class="variant rounded mt-3">
             <legend class="fieldset-label font-size-1 font-weight-bold p-2 w-auto">New Variant ${variantAmount + 1}</legend>
             <label for="variant-name-${variantAmount + 1}">Variant Name*</label>
-            <input type="text" id="variant-name-${variantAmount + 1}" name="variant-name-${variantAmount + 1}" class="w-100 d-block" maxlength="100" required>
+            <input type="text" id="variant-name-${variantAmount + 1}" name="variant-name-${variantAmount + 1}" class="form-input w-100 d-block" maxlength="100" required>
             <label for="variant-sku-${variantAmount + 1}" class="mt-2">Variant SKU</label>
-            <input type="text" id="variant-sku-${variantAmount + 1}" name="variant-sku-${variantAmount + 1}"  class="w-100 d-block" maxlength="100">
+            <input type="text" id="variant-sku-${variantAmount + 1}" name="variant-sku-${variantAmount + 1}"  class="form-input w-100 d-block" maxlength="100">
             <label for="variant-images-${variantAmount + 1}" class="mt-2 d-block">Variant Images</label>
-            <input type="file" id="variant-images-${variantAmount + 1}" name="variant-images-${variantAmount + 1}" multiple>
+            <div class="image-upload">
+                <div class="cta cta--third d-block file-input mt-3">
+                    Select Image(s) <input type="file" id="variant-images-${variantAmount + 1}" accept="image/*" name="variant-images-${variantAmount + 1}" class="images-input" multiple>
+                </div>
+                <p class="filename add-highlight mt-3"></p>
+            </div>
             <p class="info-highlight mt-3">Only .png and .jpg files are supported</p>
             <p class="info-highlight mt-3">Default image can be set on the edit page after product is saved</p>
             <button type="button" class="variant-delete cta cta--delete d-block mt-4" value="#variant-${variantAmount + 1}">Delete</button>
@@ -23,8 +28,8 @@ $('#add-variant').on('click', function(){
     $('#variant-count').val(variantCount);
 })
 
-// (Mark For) Deletion of Variant
-$(document).on('click', '.variant-delete', function(){
+// Deletion of New Variant
+$(document).on('click', '.variant-delete', function() {
     console.log('on delete ran')
     variant = $(this).val();
     variantNum = variant.split('-')[1]
@@ -70,7 +75,7 @@ $(document).on('click', '.variant-delete', function(){
     }
 })
 
-// Indicator to show which image will be set to default on edit page
+// Toggle indicator to show which image will be set to default on edit page
 $('.image-selector').on('change', function() {
     $('.image-selector').parent().find('label').removeClass('default')
     $('.image-selector:radio:checked').next('label').addClass('default');
@@ -80,3 +85,22 @@ $('.variant-image-selector').on('change', function() {
     $('.variant-image-selector').parent().find('label').removeClass('default')
     $('.variant-image-selector:radio:checked').next('label').addClass('default');
 })
+
+// Show selected product image filename
+$(document).on('change','.images-input', function() {
+    var id = $(this).attr('id')
+    var input = document.getElementById(id) // JQuery didnt work
+    console.log(input.files)
+    $(this).parent().parent().find('p').text('')
+    for (var i = 0; i < input.files.length; ++i) {
+        var name = input.files.item(i).name;
+        console.log(name)
+        if (i == 0) {
+            $(this).parent().parent().find('p').append(`Images to be uploaded on save:<br>${name}`);
+        }
+
+        else {
+            $(this).parent().parent().find('p').append(`,<br>${name}`);
+        }
+    }
+});
