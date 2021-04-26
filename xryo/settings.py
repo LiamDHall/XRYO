79083c9871af70e7df825351ccf92d86435c51fc
LiamDHall <hall.d.liam@gmail.com>
2021-04-26 20:27:18 +0000

    Remove old default sender email setting

--- xryo/settings.py
+++ xryo/settings.py
@@ -209,7 +209,6 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_SENDER_EMAIL = 'xryo@example.com'
 
 
 # Email
