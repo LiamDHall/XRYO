7bbf8d0acd482a7942cd41a5b4f574a1f5089f7d
LiamDHall <hall.d.liam@gmail.com>
2021-04-27 19:33:11 +0000

    Disable image resizing on initial upload in deployed version

--- products/models.py
+++ products/models.py
@@ -7,9 +7,7 @@ from .feilds import IntegerRangeField
 
 from PIL import Image as ImageTool
 
-from io import BytesIO
 import os
-import boto3
 
 if 'USE_AWS' not in os.environ:
     media_url = settings.MEDIA_URL.lstrip("/")
@@ -55,18 +53,10 @@ class Image(models.Model):
         all be uniform
         """
 
-        s3 = boto3.resource('s3')
-        obj = s3.Object(
-            bucket_name=os.environ["BUCKET"],
-            key=f'{media_url}{self.image}',
-        )
-        obj_body = obj.get()['Body'].read()
-        img = ImageTool.open(BytesIO(obj_body))
-        img = img.resize((1600, 2000))
-        buffer = BytesIO()
-        img.save(buffer)
-        buffer.seek(0)
-        obj.put(Body=buffer)
+        if created and 'USE_AWS' not in os.environ:
+            image = ImageTool.open(f'{media_url}{self.image}')
+            image = image.resize((1600, 2000))
+            image.save(f'{media_url}{self.image}')
 
     def set_default(self):
         """Set default the sender image as default of
